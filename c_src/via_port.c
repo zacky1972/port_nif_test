3f95bd851e0634213ef7f48e0698d1a030f78233
Susumu Yamazaki <zacky1972@gmail.com>
2021-01-20 09:26:06 +0900

    create via_port and add size

--- c_src/via_port.c
+++ c_src/via_port.c
@@ -2,18 +2,26 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define N 600000
-
-int main()
+int main(int argc, const char *argv[])
 {
-	int64_t *vec_long = (int64_t *)malloc(sizeof(int64_t) * N);
+	if(argc != 2) {
+		return 1;
+	}
+
+	uint64_t size;
+	if(!sscanf(argv[1], "%llu", &size)) {
+		return 1;
+	}
+
+	int64_t *vec_long = (int64_t *)malloc(sizeof(int64_t) * size);
 	if(__builtin_expect(vec_long == NULL, false)) {
 		return 1;
 	}
-	for(int64_t i = 0; i < N; i++) {
+	for(int64_t i = 0; i < size; i++) {
 		vec_long[i] = i;
 	}
-	kernel(vec_long, N);
+	kernel(vec_long, size);
 	return 0;
 }
--- lib/port_nif_test.ex
+++ lib/port_nif_test.ex
@@ -1,2 +1,20 @@
 defmodule PortNifTest do
+  def via_port(size) do
+  	ViaPort.start_link(size)
+  	receive_get()
+  end
+
+  def receive_get() do
+    ret = ViaPort.get()
+
+    case ret do
+      {:ok, result} -> {:ok, result}
+
+      :not_yet ->
+        Process.sleep(50) 
+        receive_get()
+
+      :error -> :error
+    end
+  end
 end

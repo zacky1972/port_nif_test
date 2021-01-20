0ba9e98175bb7f610f688374f6862f4b0306f888
Susumu Yamazaki <zacky1972@gmail.com>
2021-01-20 09:16:59 +0900

    apply mix format

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PortNifTest.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      compilers: [:elixir_make] ++ Mix.compilers,
+      compilers: [:elixir_make] ++ Mix.compilers(),
       make_clean: ["clean"],
       deps: deps()
     ]
